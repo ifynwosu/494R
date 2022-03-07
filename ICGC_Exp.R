161be33357d187ca20d0454e833af03678825b82
tablerd <99215896+tablerd@users.noreply.github.com>
2022-03-07 12:08:22 -0700

    Add files via upload

--- ICGC_Exp.R
+++ ICGC_Exp.R
@@ -1,6 +1,13 @@
 library(tidyverse)
 source("functions/removeColsWithOnlyOneValue.R")
 
-download.file("https://dcc.icgc.org/api/v1/download?fn=/current/Projects/BRCA-KR/exp_seq.BRCA-KR.tsv.gz", destfile = "~")
-exp_seq <- read_tsv("exp_seq.BRCA-KR.tsv.gz") %>%
+#create or utilize directory
+data_Dir <- "dataDir"
+if (!dir.exists(data_Dir)) {
+  dir.create(data_Dir)
+}
+
+#download expression file from this URL
+download.file("https://dcc.icgc.org/api/v1/download?fn=/current/Projects/BRCA-KR/exp_seq.BRCA-KR.tsv.gz", destfile = "dataDir")
+exp_seq <- read_tsv("dataDir/exp_seq.BRCA-KR.tsv.gz") %>%
   removeColsWithOnlyOneValue()
