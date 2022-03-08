733ebf981345d1f78dcb63df56777846abe7cdd0
tablerd <99215896+tablerd@users.noreply.github.com>
2022-03-07 17:59:04 -0700

    Add files via upload

--- MetaBric_Exp.R
+++ MetaBric_Exp.R
@@ -1,3 +1,13 @@
 library(tidyverse)
+library(janitor)
 
-metaBric_exp <- read_tsv("https://media.githubusercontent.com/media/cBioPortal/datahub/master/public/brca_metabric/data_mrna_agilent_microarray.txt")
+#read expression file from this URL
+metaBric_exp <- read_tsv("https://media.githubusercontent.com/media/cBioPortal/datahub/master/public/brca_metabric/data_mrna_agilent_microarray_zscores_ref_diploid_samples.txt")
+
+#remove rows with greater than or equal to 25% NA values
+for(i in 1:nrow(metaBric_exp)) {
+  if((sum(is.na(metaBric_exp[i,]))/ncol(metaBric_exp)) >= 0.25) {
+    metaBric_exp[i,] <- list(NA)
+  }
+}
+metaBric_exp <- remove_empty(metaBric_exp, "rows")
